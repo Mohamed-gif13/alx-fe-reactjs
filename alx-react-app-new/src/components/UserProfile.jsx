69658bb88f1e05dfd0b835934dd55862a0d7f1cd
Mohamed Al Ibrahimi <alibrahimi.mohamed.13@gmail.com>
2025-02-22 18:43:04 +0100

    add all

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -6,8 +6,30 @@ function UserProfile() {
 
   return (
     <div>
-      <h2>{userData.name}</h2>
-      <p>Email: {userData.email}</p>
+      <h2>
+        <span
+          style={{
+            color: 'blue',     // Appliquer la couleur bleue
+            border: '2px solid blue',  // Appliquer une bordure bleue de 2px
+            padding: '10px',   // Ajouter du padding de 10px
+            margin: '10px',    // Ajouter une marge de 10px
+          }}
+        >
+          {userData.name}
+        </span>
+      </h2>
+      <p>
+        Email: <span
+          style={{
+            color: 'blue',
+            border: '2px solid blue',
+            padding: '10px',
+            margin: '10px',
+          }}
+        >
+          {userData.email}
+        </span>
+      </p>
     </div>
   );
 }
