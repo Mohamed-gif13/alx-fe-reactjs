6fcbd979162ab944ba6e1087cc64442127f8a670
Mohamed Al Ibrahimi <alibrahimi.mohamed.13@gmail.com>
2025-03-03 13:05:41 +0000

    add all

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import RecipeList from './components/RecipeList';  // Importation du composant RecipeList
+import AddRecipeForm from './components/AddRecipeForm';  // Importation du composant AddRecipeForm
 
 const App = () => {
   return (
     <div>
       <h1>Application de partage de recettes</h1>
-      <RecipeList />  {/* Affichage du composant RecipeList */}
+      <AddRecipeForm />  {/* Affichage du formulaire pour ajouter une recette */}
+      <RecipeList />  {/* Affichage de la liste des recettes */}
     </div>
   );
 };
