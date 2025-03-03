deebc1c49179a3e79d59770d0c11ac77e9081f91
Mohamed Al Ibrahimi <alibrahimi.mohamed.13@gmail.com>
2025-03-03 13:27:29 +0000

    add all

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import RecipeList from './components/RecipeList';
 import SearchBar from './components/SearchBar';
+import AddRecipeForm from './components/AddRecipeForm';  // Importation ajoutée ici
 
 const App = () => {
   return (
     <div>
       <h1>Recipe Sharing App</h1>
       <SearchBar /> {/* Composant pour la barre de recherche */}
+      <AddRecipeForm /> {/* Composant pour ajouter une recette */}
       <RecipeList /> {/* Composant pour afficher les recettes filtrées */}
     </div>
   );
