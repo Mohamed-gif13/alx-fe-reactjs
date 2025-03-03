7889fb456041df9dccd6c3bf5c711cea694a929a
Mohamed Al Ibrahimi <alibrahimi.mohamed.13@gmail.com>
2025-03-03 13:00:10 +0000

    add all files into the folder

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,15 @@
-import RecipeList from './components/recipeStore.js';
-import AddRecipeForm from './components/AddRecipeForm';
+// src/App.jsx
 
-function App() {
+import React from 'react';
+import RecipeList from './components/RecipeList';  // Importation du composant RecipeList
+
+const App = () => {
   return (
     <div>
-      <h1>Application de Partage de Recettes</h1>
-      <AddRecipeForm />
-      <RecipeList />
+      <h1>Application de partage de recettes</h1>
+      <RecipeList />  {/* Affichage du composant RecipeList */}
     </div>
   );
-}
+};
 
 export default App;
