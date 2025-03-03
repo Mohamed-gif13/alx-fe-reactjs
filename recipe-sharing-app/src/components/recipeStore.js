d408282f88d9898af118d9bf453fec9a4c0ddb7d
Mohamed Al Ibrahimi <alibrahimi.mohamed.13@gmail.com>
2025-03-03 13:17:46 +0000

    add all

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,15 +1,25 @@
-// src/components/recipeStore.js
 import create from 'zustand';
 
-export const useRecipeStore = create(set => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (recipe) => set(state => ({ recipes: [...state.recipes, recipe] })),
-  deleteRecipe: (id) => set(state => ({
-    recipes: state.recipes.filter(recipe => recipe.id !== id)
+  
+  // Action pour ajouter une recette
+  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
+  
+  // Action pour mettre à jour une recette
+  updateRecipe: (id, updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+    ),
   })),
-  updateRecipe: (updatedRecipe) => set(state => ({
-    recipes: state.recipes.map(recipe =>
-      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-    )
-  }))
+  
+  // Action pour supprimer une recette
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== id),
+  })),
+  
+  // Action pour définir toutes les recettes
+  setRecipes: (recipes) => set({ recipes }),  // Ajoute cette fonction pour définir les recettes
 }));
+
+export { useRecipeStore };
