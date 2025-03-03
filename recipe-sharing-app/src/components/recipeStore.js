4636729d76436af6d37e8abf47fadc9b73d10f99
Mohamed Al Ibrahimi <alibrahimi.mohamed.13@gmail.com>
2025-03-03 12:54:26 +0000

    add new files

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (recipe) =>
+    set((state) => ({ recipes: [...state.recipes, recipe] })),
+  removeRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+  clearRecipes: () => set({ recipes: [] }),
+}));
+
+export default useRecipeStore;
