fcf2db9a4ad3e7bd92c115b8bc8018b4623b0167
Mohamed Al Ibrahimi <alibrahimi.mohamed.13@gmail.com>
2025-03-04 23:51:40 +0000

    add all files

---
+++ react-todo/src/components/TodoList.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import TodoItem from "./TodoItem";
+import AddTodoForm from "./AddTodoForm";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Apprendre React", completed: false },
+    { id: 2, text: "Faire les courses", completed: true },
+  ]);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { id: Date.now(), text, completed: false }]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h2>Liste des Tâches</h2>
+      <AddTodoForm addTodo={addTodo} />
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
---
+++ react-todo/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';  // Assurez-vous que le chemin du fichier CSS est correct
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
