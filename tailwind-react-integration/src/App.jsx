2c8539cbba1e54da0f4fbc5db211391ecdbf9526
Mohamed Al Ibrahimi <alibrahimi.mohamed.13@gmail.com>
2025-03-17 17:53:08 +0000

    add all files into the folder

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserProfile from "./components/UserProfile"; // Adjust the path if needed
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
